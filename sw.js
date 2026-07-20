73d426de072e63ac81a8bf9ef4e6e195e0ad4ac3
saramimoriawase <i08510.09e1317@gmail.com>
2026-07-20 19:49:37 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,87 @@
+const CACHE_NAME = "mahjong-score-v1";
+
+const APP_FILES = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => cache.addAll(APP_FILES))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) =>
+        Promise.all(
+          cacheNames
+            .filter((name) => name !== CACHE_NAME)
+            .map((name) => caches.delete(name))
+        )
+      )
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      fetch(event.request)
+        .then((response) => {
+          const copy = response.clone();
+
+          caches
+            .open(CACHE_NAME)
+            .then((cache) =>
+              cache.put("./index.html", copy)
+            );
+
+          return response;
+        })
+        .catch(() =>
+          caches.match("./index.html")
+        )
+    );
+
+    return;
+  }
+
+  const requestUrl = new URL(event.request.url);
+
+  if (requestUrl.origin !== self.location.origin) {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) {
+        return cached;
+      }
+
+      return fetch(event.request).then((response) => {
+        const copy = response.clone();
+
+        caches
+          .open(CACHE_NAME)
+          .then((cache) =>
+            cache.put(event.request, copy)
+          );
+
+        return response;
+      });
+    })
+  );
+});
